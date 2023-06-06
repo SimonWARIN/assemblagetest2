afa399680d4de6c5e25f9f9f9bc760cc63aec393
SimonWARIN <sim.warin@gmail.com>
2023-06-06 12:14:52 +0200

    commit du 06/06

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 
-const int trigPin = 18;
-const int echoPin = 5;
+const int trigPin1 = 5;
+const int echoPin1 = 18;
+
+const int trigPin2 = 0;
+const int echoPin2 = 4;
 
 const int channel = 0; // Canal PWM à utiliser
 const int frequency = 50;// Fréquence PWM en Hz const
@@ -13,14 +16,20 @@ const int pwmPin = 13; // Broche de sortie PWM
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
 
-long duration;
-float distanceCm;
-float distanceInch;
+long duration1;
+float distanceCm1;
+float distanceInch1;
+
+long duration2;
+float distanceCm2;
+float distanceInch2;
 
 void setup() {
   Serial.begin(115200); // Starts the serial communication
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
 
 // Configuration de la bibliothèque ledc
 ledcSetup(channel, frequency, resolution);
@@ -29,36 +38,53 @@ ledcAttachPin(pwmPin, channel);
 
 void loop() {
   // Clears the trigPin
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin1, LOW);
   delayMicroseconds(2);
-  
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin1, LOW);
 
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration1 = pulseIn(echoPin1, HIGH);
+  // Calculate the distance
+  distanceCm1 = duration1 * SOUND_SPEED/2;
+  // Convert to inches
+  distanceInch1 = distanceCm1 * CM_TO_INCH;
 
-// Calculate the distance
-distanceCm = duration * SOUND_SPEED/2;
+  // Clears the trigPin
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
 
-// Convert to inches
-distanceInch = distanceCm * CM_TO_INCH;
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration2 = pulseIn(echoPin2, HIGH);
+  // Calculate the distance
+  distanceCm2 = duration2 * SOUND_SPEED/2;
+  // Convert to inches
+  distanceInch2 = distanceCm2 * CM_TO_INCH;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance1 (cm): ");
+  Serial.println(distanceCm1);
+  Serial.print("Distance1 (inch): ");
+  Serial.println(distanceInch1);
 
-// Prints the distance in the Serial Monitor
-Serial.print("Distance (cm): ");
-Serial.println(distanceCm);
-Serial.print("Distance (inch): ");
-Serial.println(distanceInch);
+  Serial.print("Distance2 (cm): ");
+  Serial.println(distanceCm2);
+  Serial.print("Distance2 (inch): ");
+  Serial.println(distanceInch2);
 
-if(distanceCm<30) {
-  ledcWrite(channel, 13);
-}
+  if(distanceCm1<30) {
+    ledcWrite(channel, 13);
+  }
 
-else {
-  ledcWrite(channel, 23);
-}
+  else {
+    ledcWrite(channel, 23);
+  }
 
-delay(1000);
+  delay(1000);
 }
